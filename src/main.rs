947c4447525bd15e8a56b42413455133ad883ef3
sauloluas <sauloroba@gmail.com>
2025-02-22 11:36:41 -0300

    Render prototype working

--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,25 @@
-struct Entity {
-    name: String,
-    health: u8,
-    signs: Vec<Sign>,
-    moves: Vec<Move>,
-}
+mod tools;
+use tools::*;
 
-impl Entity {
-    fn new(name: String, health: u8) -> Self {
-        Self {
-            name,
-            health,
-            signs: Vec::new(),
-            moves: Vec::new(),
-        }
-    }
-
-    fn render(&self) {
-        println!("    {}", self.name);
-        println!("    HP: {}", self.health);
-        print!("    ");
-        for sign in &self.signs {
-            // print!("{}", sign.shortname);
-            sign.render();
-        }
-        print!("\n");
-        print!("\n");
-    }
-}
-
-struct Sign {
-    name: String,
-    shortname: String,
-    color: Color,
-}
-
-impl Sign {
-    fn new(name: String, shortname: String, color: Color) -> Self {
-        Self {
-            name,
-            shortname,
-            color,
-        }
-    }
-
-    fn render(&self) {
-        print!("\x1b[{}m{}\x1b[0m", self.color.get_code(), self.shortname);
-    }
-}
-
-struct Move {
-    name: String,
-    damage: Dice,
-    effect: Sign,
-    strength: Application,
-}
-
-enum Dice {
-    D4,
-    D6,
-}
-
-enum Color {
-    NoColor,
-    Black,
-    Red,
-    Green,
-    Yellow,
-    Blue,
-    Magenta,
-    Cyan,
-    White,
-    Gray,
-    BrightRed,
-    DodgerBlue
-}
-
-impl Color {
-    fn get_code(&self) -> &str {
-        match &self {
-            Self::Black => "30",
-            Self::Red => "31",
-            Self::Green => "32",
-            Self::Yellow => "33",
-            Self::Blue => "34",
-            Self::Magenta => "35",
-            Self::Cyan => "36",
-            Self::White => "37",
-            Self::Gray => "90",
-            Self::BrightRed => "91",
-            Self::DodgerBlue => "38;2;30;144;255",
-            Self::NoColor => "Not implemented",
-        }
-    }
-}
-
-enum Application {
-    Weak,
-    Strong,
-}
 
 fn main() {
-    let mut gunter = Entity::new(String::from("Hunter"), 19);
 
-    let queimado = Sign::new(String::from("Burning"), String::from("Brn"), Color::Red);
+    let mut field = Field {
+        party: vec![EntityType::Hunter.instance(), 
+    EntityType::Orbat.instance()],
+    
+        gamemaster: vec![EntityType::Butler.instance(),
+    EntityType::Butler.instance()]
+    };
 
-    gunter.signs.push(queimado);
 
-    let molhado = Sign::new(String::from("Wet"), String::from("Wet"), Color::Blue);
 
-    gunter.signs.push(molhado);
+    // field.party[0].signs.push(SignType::Wet.instance());
 
-    let envenenado = Sign::new(String::from("Poisoned"), String::from("Psn"), Color::Green);
+    // field.party[0].signs.push(SignType::Burning.instance());
 
-    gunter.signs.push(envenenado);
+    field.render();
 
-    gunter.render();
-
-    let mut orbat = Entity::new(String::from("Orbat"), 5);
-
-    orbat.render();
 }
+
+
---
+++ src/tools.rs
@@ -0,0 +1,239 @@
+pub struct Entity {
+    pub name: &'static str,
+    pub health: u8,
+    pub signs: Vec<Sign>,
+    pub moves: Vec<Move>,
+}
+
+impl Entity {
+
+    pub fn render(&self) {
+        println!("    {}", self.name);
+        println!("      HP: {}", self.health);
+        print!("      ");
+        for sign in &self.signs {
+            // print!("{}", sign.shortname);  DEPRECATED
+            sign.render();
+        }
+        print!("\n");
+        print!("\n");
+    }
+}
+
+pub enum EntityType {
+    Hunter,
+    Orbat,
+    Vampire,
+    Butler
+}
+
+impl EntityType {
+    pub fn instance(&self) -> Entity {
+        match &self {
+            EntityType::Hunter => Entity {
+                name: "Hunter",
+                health: 18,
+                signs: Vec::new(),
+                moves: vec![MoveType::Slash.instance()]
+
+            },
+            EntityType::Orbat => Entity {
+                name: "Orbat",
+                health: 8,
+                signs: Vec::new(),
+                moves: vec![MoveType::Pull.instance()]
+            },
+            EntityType::Butler => Entity {
+                name: "Butler",
+                health: 13,
+                signs: Vec::new(),
+                moves: vec![]
+            },
+            _ => Entity{
+                name: "Unknown",
+                health: 1,
+                signs: Vec::new(),
+                moves: Vec::new()
+            }
+        }
+    }
+}
+
+
+
+
+
+pub struct Field {
+    pub party: Vec<Entity>,
+    pub gamemaster: Vec<Entity>
+}
+
+impl Field {
+    pub fn render(&self) {
+        let mut count: usize = 0;
+
+        for entity in &self.gamemaster {
+            print!("{})", KEY[count]);
+            count += 1;
+            entity.render();
+        }
+
+        print!("--------------------------\n");
+        print!("\n");
+        print!("\n");
+
+        for entity in &self.party {
+            print!("{})", KEY[count]);
+            count += 1;
+            entity.render();
+        }
+
+    }
+}
+
+
+const KEY: [char; 10] = ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c', 'v'];
+
+
+pub struct Sign {
+    pub name: &'static str,
+    pub shortname: &'static str,
+    pub color: Color,
+}
+
+impl Sign {
+    pub fn render(&self) {
+        print!("\x1b[{}m{}\x1b[0m", self.color.get_code(), self.shortname);  // Future Works: implement Display trait
+    }
+}
+
+pub enum SignType {
+    NoEffect,
+    Burning,
+    Wet,
+    Poisoned,
+    Cut
+}
+
+impl SignType {
+    pub fn instance(&self) -> Sign {                    // Future Works: possibility of adding HashMap
+        match &self {
+            SignType::NoEffect => Sign {
+                name: "No effect",
+                shortname: "Nef",
+                color: Color::White
+            },
+            SignType::Burning => Sign {
+                name: "Burning",
+                shortname: "Brn",
+                color: Color::Red
+            },
+            SignType::Wet => Sign {
+                name: "Wet",
+                shortname: "Wet",
+                color: Color::Blue
+            },
+            SignType::Poisoned => Sign {
+                name: "Poisoned",
+                shortname: "Psn",
+                color: Color::Green
+            },
+            SignType::Cut => Sign {
+                name: "Cut",
+                shortname: "Cut",
+                color: Color::Gray
+            }
+        }
+    }
+}
+
+
+
+pub enum Color {
+    NoColor,
+    Black,
+    Red,
+    Green,
+    Yellow,
+    Blue,
+    Magenta,
+    Cyan,
+    White,
+    Gray,
+    BrightRed,
+    DodgerBlue
+}
+
+impl Color {
+    pub fn get_code(&self) -> &str {
+        match &self {
+            Self::Black => "30",
+            Self::Red => "31",
+            Self::Green => "32",
+            Self::Yellow => "33",
+            Self::Blue => "34",
+            Self::Magenta => "35",
+            Self::Cyan => "36",
+            Self::White => "37",
+            Self::Gray => "90",
+            Self::BrightRed => "91",
+            Self::DodgerBlue => "38;2;30;144;255",
+            Self::NoColor => "Not implemented",
+        }
+    }
+}
+
+
+
+
+
+
+
+
+pub struct Move {
+    pub name: &'static str,
+    pub damage: Dice,
+    pub effect: Sign,
+    pub strength: Application,
+}
+
+pub enum MoveType {
+    Slash,
+    Pull
+}
+
+impl MoveType {
+    pub fn instance(&self) -> Move {
+        match &self {
+            MoveType::Slash => Move {
+                name: "Slash",
+                damage: Dice::D4,
+                effect: SignType::Cut.instance(),
+                strength: Application::Weak
+            },
+            MoveType::Pull => Move {
+                name: "Pull",
+                damage: Dice::D4,
+                effect: SignType::NoEffect.instance(),
+                strength: Application::Intensifier
+            }
+        }
+    }
+}
+
+
+
+
+
+pub enum Dice {
+    Coin,
+    D4,
+    D6,
+}
+
+
+pub enum Application {
+    Weak,
+    Strong,
+    Intensifier
+}
